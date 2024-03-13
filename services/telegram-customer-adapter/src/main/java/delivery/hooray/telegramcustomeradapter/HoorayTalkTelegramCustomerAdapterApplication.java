fcd30b3143882a4e195882f00697ef3ee5619956
Artur Vashchenkov <washenkov@gmail.com>
2024-03-13 01:29:03 +0100

    telegram-customer-adapter service skeleton is created

---
+++ services/telegram-customer-adapter/src/main/java/delivery/hooray/telegramcustomeradapter/HoorayTalkTelegramCustomerAdapterApplication.java
@@ -0,0 +1,13 @@
+package delivery.hooray.telegramcustomeradapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoorayTalkTelegramCustomerAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoorayTalkTelegramCustomerAdapterApplication.class, args);
+	}
+
+}
