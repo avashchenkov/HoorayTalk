a3293ed8f22e73e7985eb7d27e11639fe9725194
Artur Vashchenkov <washenkov@gmail.com>
2024-03-27 22:10:45 +0100

    App class name shortened

--- services/core/message-hub/src/main/java/delivery/hooray/messagehub/MessageHubApplication.java
+++ services/core/message-hub/src/main/java/delivery/hooray/messagehub/MessageHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HoorayTalkMessageHubApplication {
+public class MessageHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HoorayTalkMessageHubApplication.class, args);
+		SpringApplication.run(MessageHubApplication.class, args);
 	}
 
 }
