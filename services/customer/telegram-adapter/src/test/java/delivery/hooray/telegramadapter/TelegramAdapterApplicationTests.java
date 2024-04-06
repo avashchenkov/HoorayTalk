888a6dd34240a8b840b3a079c689d8e8c033c16b
Artur Vashchenkov <washenkov@gmail.com>
2024-04-06 23:07:56 +0200

    Enable mocks for the Base Test class

--- services/customer/telegram-adapter/src/test/java/delivery/hooray/telegramadapter/TelegramAdapterApplicationTests.java
+++ services/customer/telegram-adapter/src/test/java/delivery/hooray/telegramadapter/TelegramAdapterApplicationTests.java
@@ -1,13 +1,21 @@
 package delivery.hooray.telegramadapter;
 
-import org.junit.jupiter.api.Test;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class TelegramAdapterApplicationTests {
+@AutoConfigureMockMvc
+public abstract class TelegramAdapterApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	protected MockMvc mockMvc;
+
+	@BeforeEach
+	public void setup() {
+		RestAssuredMockMvc.mockMvc(mockMvc);
 	}
-
 }
