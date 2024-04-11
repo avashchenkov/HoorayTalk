e9b32862e26e2540eb402bcb45a8bc5e75ec499b
Artur Vashchenkov <washenkov@gmail.com>
2024-04-11 23:05:34 +0200

    Liquibase is initially set up

--- services/customer/telegram-adapter/src/main/java/delivery/hooray/telegramadapter/model/Bot.java
+++ services/customer/telegram-adapter/src/main/java/delivery/hooray/telegramadapter/model/Bot.java
@@ -1,32 +1,38 @@
 package delivery.hooray.telegramadapter.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "bot")
 public class Bot {
     @Id
-    @Column(name = "id", nullable = false, length = 500)
-    private String id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
 
-    @Column(name = "token_encrypted", nullable = false, unique = true, length = 500)
-    private String tokenEncrypted;
+    @Lob
+    @Column(name = "token_encrypted", nullable = false, columnDefinition = "bytea")  // Указываем точный тип данных для PostgreSQL
+    private byte[] tokenEncrypted;
 
-    public Bot(String id, String tokenEncrypted) {
-        this.id = id;
+    public Bot() {}
+
+    public Bot(byte[] tokenEncrypted) {
         this.tokenEncrypted = tokenEncrypted;
     }
 
-    protected Bot() {}
-
-    public String getId() {
+    public UUID getId() {
         return id;
     }
 
-    public String getTokenEncrypted() {
+    public byte[] getTokenEncrypted() {
         return tokenEncrypted;
+    }
+
+    public void setTokenEncrypted(byte[] tokenEncrypted) {
+        this.tokenEncrypted = tokenEncrypted;
     }
 }
