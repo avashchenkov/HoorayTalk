33c6a19b780e848f4a3ae8d172b7393272632e5f
Artur Vashchenkov <washenkov@gmail.com>
2024-05-21 20:45:19 +0200

    ApiKeyAuthFilter implemented

---
+++ services/core/message-hub/src/main/java/delivery/hooray/messagehub/security/ApiKeyAuthFilter.java
@@ -0,0 +1,39 @@
+package delivery.hooray.messagehub.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+    private final String apiKey;
+
+    public ApiKeyAuthFilter(@Value("${API_KEY}")String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String requestApiKey = request.getHeader("X-API-KEY");
+        if (apiKey.equals(requestApiKey)) {
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken("apiUser", null, null);
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
