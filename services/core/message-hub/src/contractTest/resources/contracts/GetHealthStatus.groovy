a3fd09960504d3cd751999b9c235a1aaad6da1bb
Artur Vashchenkov <washenkov@gmail.com>
2024-05-21 18:47:27 +0200

    Health Contract minor refactoring

--- services/core/message-hub/src/contractTest/resources/contracts/GetHealthStatus.groovy
+++ services/core/message-hub/src/contractTest/resources/contracts/GetHealthStatus.groovy
@@ -3,10 +3,7 @@ package contracts
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
-    description("""
-        A contract that tests the health endpoint with a 200 response.
-    """)
-
+    description "Service health check"
     request {
         method 'GET'
         url '/api/v1/health'
