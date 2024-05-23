12fdbb318c37aa1e44539231d409438100d9e964
Artur Vashchenkov <washenkov@gmail.com>
2024-05-23 21:21:49 +0200

    Message service refined to use Telegram and Discord sender services

--- services/core/message-hub/src/main/java/delivery/hooray/messagehub/service/MessageService.java
+++ services/core/message-hub/src/main/java/delivery/hooray/messagehub/service/MessageService.java
@@ -2,8 +2,10 @@ package delivery.hooray.messagehub.service;
 
 import delivery.hooray.messagehub.model.ChatModel;
 import delivery.hooray.messagehub.model.MessageModel;
+import delivery.hooray.messagehub.model.TenantModel;
 import delivery.hooray.messagehub.repository.ChatRepository;
 import delivery.hooray.messagehub.repository.MessageRepository;
+import delivery.hooray.messagehub.repository.TenantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,21 @@ public class MessageService {
 
     private final ChatRepository chatRepository;
     private final MessageRepository messageRepository;
+    private final DiscordSenderService discordSenderService;  // TODO:
+    private final TelegramSenderService telegramSenderService;
+    private final TenantRepository tenantRepository;
 
     @Autowired
     public MessageService(ChatRepository chatRepository,
-                          MessageRepository messageRepository) {
+                          MessageRepository messageRepository,
+                          DiscordSenderService discordSenderService,
+                          TelegramSenderService telegramSenderService,
+                          TenantRepository tenantRepository) {
         this.chatRepository = chatRepository;
         this.messageRepository = messageRepository;
+        this.discordSenderService = discordSenderService;
+        this.telegramSenderService = telegramSenderService;
+        this.tenantRepository = tenantRepository;
     }
 
     public void handleCustomerMessage(CustomerMessageDto messageDto) {
@@ -27,6 +38,17 @@ public class MessageService {
         messageRepository.save(messageModel);
 
         System.out.println("Handling customer message: " + messageDto.getMessage());
+
+        TenantModel tenantModel = chatModel.getTenant();
+
+        MessageToDiscordAdapterRequestData message = new MessageToDiscordAdapterRequestData(messageDto.getMessage(),
+                                                                                            chatModel.getAdminChatId(),
+                                                                                            tenantModel.getAdminBot().getId().toString());
+
+        discordSenderService.sendMessage(message).subscribe(
+                result -> System.out.println("Operation successful: " + result),
+                error -> System.err.println("Operation failed: " + error.getMessage())
+        );
     }
 
     public void handleAdminMessage(AdminMessageDto messageDto) {
@@ -36,6 +58,17 @@ public class MessageService {
         messageRepository.save(messageModel);
 
         System.out.println("Handling admin message: " + messageDto.getMessage());
+
+        TenantModel tenantModel = chatModel.getTenant();
+
+        MessageToTelegramAdapterRequestData message = new MessageToTelegramAdapterRequestData(messageDto.getMessage(),
+                chatModel.getCustomerChatId(),
+                tenantModel.getCustomerBot().getId().toString());
+
+        telegramSenderService.sendMessage(message).subscribe(
+                result -> System.out.println("Operation successful: " + result),
+                error -> System.err.println("Operation failed: " + error.getMessage())
+        );
     }
 
     protected ChatModel getChatModel(CustomerMessageDto customerMessageDto) {
