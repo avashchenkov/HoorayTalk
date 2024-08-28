8bc3a44a4005bca4d41f5ec47c8b42e4496076ec
Artur Vashchenkov <washenkov@gmail.com>
2024-08-29 00:04:04 +0200

    Add UserAiAssistantInstruction after the end of the AiAssistantInstruction as an AI system prompt

--- services/core/message-hub/src/main/java/delivery/hooray/messagehub/service/ai/AiAssistantMessageService.java
+++ services/core/message-hub/src/main/java/delivery/hooray/messagehub/service/ai/AiAssistantMessageService.java
@@ -57,7 +57,8 @@ public class AiAssistantMessageService {
 
         completeChatRequest.setRecentMessages(currentOrderMessages);
 
-        String systemPrompt = chatModel.getAiAssistantInstruction().getText();
+        String systemPrompt = chatModel.getAiAssistantInstruction().getText() + "\n" +
+                              chatModel.getTenant().getUserAiAssistantInstruction().getText();
 
         completeChatRequest.setSystemMessage(systemPrompt);
 
